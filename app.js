c6aa8995ee10c0e6086b485a36e33d26b75d818b
Ziad <ZiadElsayedAsr@gmail.com>
2024-01-28 09:54:32 +0200

    update

--- app.js
+++ app.js
@@ -3,15 +3,7 @@ const express = require('express');
 
 const app = express();
 
-// app.get('/', (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: 'Hello from the server side...', app: 'Natours' });
-// });
-
-// app.post('/', (req, res) => {
-//   res.send('You can post to this endpoint...');
-// });
+app.use(express.json()); // A middleware
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -27,6 +19,26 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newID = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newID }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    // Must be (async) here because it is inside a callback function (Event loop lecture)
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours), // Convert it into json before writing it
+    (err) => {
+      res.status(201).send({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
