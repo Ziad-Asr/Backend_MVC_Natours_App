441d91d729efe7140e63a8377a151199ee6f16de
Ziad <ZiadElsayedAsr@gmail.com>
2024-02-01 09:17:30 +0200

    update

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: './config.env' }); // Should be written before importing {{app}}
+
 const app = require('./app');
 
-console.log(process.env.NODE_ENV);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWRD,
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log(`DB connection is successfully established`);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
