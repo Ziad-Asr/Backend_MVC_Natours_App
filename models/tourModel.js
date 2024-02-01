de7850cdd08d48067f9092c2a858ef24611975d0
Ziad <ZiadElsayedAsr@gmail.com>
2024-02-01 19:37:15 +0200

    update

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,16 +5,53 @@ const toursSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour must have a name'],
+    trim: true,
     unique: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'The tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'The tour must have a maxGroupSize'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'The tour must have a difficulty'],
+  },
+  ratingAverage: {
     type: String,
     default: 4.5,
+  },
+  ratingQuantity: {
+    type: String,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDicount: String,
+  summary: {
+    type: String,
+    trim: true, //Remove all space from the sides of a string
+    required: [true, 'A tour must have a summary'],
+  },
+  description: {
+    type: String,
+    trim: true, //Remove all space from the sides of a string
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a imageCover'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDate: [Date],
 });
 
 // Creating a collection
