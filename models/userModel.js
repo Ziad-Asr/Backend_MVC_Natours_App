7029acd3c0d199b3ae59a7492f416acb30eb6216
Ziad <ZiadElsayedAsr@gmail.com>
2024-02-09 18:42:07 +0200

    update

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -23,7 +24,24 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please, confirm your password'],
     minlength: 8,
+    validate: {
+      // This onl works on ((create(user) => post & save)) not findOne or findOneAndUpdate for example.
+      validator: function (el) {
+        return el === this.password;
+        // Must return true or false
+      },
+      message: 'Password are not the same',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next(); // Will noly run if the password is modified
+
+  this.password = await bcrypt.hash(this.password, 12); // Used to hash(encrypt) the password
+  this.passwordConfirm = undefined; // Delete the value of passwordConfirm => because it only used for verification not to be saved in the database
+
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
